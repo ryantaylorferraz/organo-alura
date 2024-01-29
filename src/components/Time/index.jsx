2983c7d795519f7f7e5d877eeb6668c69b961e35
ryan <ryan.bilac2010@hotmail.com>
2024-01-29 09:08:25 -0300

    projeto terminado

---
+++ src/components/Time/index.jsx
@@ -0,0 +1,26 @@
+import Colaborador from "../Colaborador";
+import "./time.css";
+
+export const Time = (props) => {
+  const css = { backgroundColor: props.corSecundaria };
+
+  return props.colaboradores.length > 0 ? (
+    <section className="time" style={css}>
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => (
+          <Colaborador
+            corDeFundo={props.corPrimaria}
+            key={colaborador.nome}
+            nome={colaborador.nome}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+          />
+        ))}
+      </div>
+    </section>
+  ) : null;
+};
+
+
+// (props.colaboradores.length > 0) ? ()      : ""
